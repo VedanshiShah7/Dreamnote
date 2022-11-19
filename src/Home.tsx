9eeac02c287de235eaebcd6037b80a039ce08dcd
Ariana Lozner <arianalozner@Arianas-MacBook-Pro.local>
2022-11-19 17:46:16 -0500

    home

--- src/Home.tsx
+++ src/Home.tsx
@@ -17,20 +17,20 @@ function Home() {
 
 
     {/* </Center> */}
-    <Text px='10rem' fontSize='xl'>Hello! We are Vedanshi, Ariana and Ava and we created Dreamnote! Dreamnote is a platform to take notes and get summarized information with key elements on the notes you have taken. For example, if you are taking notes for class and need to study for a test, you can use our functionality to store those notes and get helpful summaries to prep for exams more efficiently.
+    <Text px='10rem' fontSize='xl' align='center'>Hello! We are Vedanshi, Ariana and Ava and we created Dreamnote! Dreamnote is a platform to take notes and get summarized information with key elements on the notes you have taken. For example, if you are taking notes for class and need to study for a test, you can use our functionality to store those notes and get helpful summaries to prep for exams more efficiently.
 </Text>
-<Text px='10rem' py='1rem' fontSize='xl'>
+<Text px='10rem' py='1rem' align='center' fontSize='xl'>
 
 We created this web application during the IWC hackathon - “Hack the Patriarchy” that took place at Northeastern University on Saturday, November 19, 2022.
 </Text>
 
 <Text px='10rem' fontSize='xl'>
 We hope you enjoy the product!</Text>
+
   
          </VStack>
 
 
-       
 
   </div>
   )
