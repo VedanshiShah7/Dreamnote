7348d83822dc61a6642b14de8f07c84bd8bf9a55
Ariana Lozner <arianalozner@Arianas-MacBook-Pro.local>
2022-11-19 11:47:31 -0500

    api stuff'

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,33 @@ function App() {
     let inputValue = e.target.value
     setValue(inputValue)
   }
+
+
+  const params = {
+    method: 'POST',
+    //key: 'c227dd46f1329bdf28e5610f3fd9cc5a',
+    lang: 'English',
+    txt: "Cellular respiration is a series of chemical reactions that break down glucose to produce ATP, which may be used as energy to power many reactions throughout the body. There are three main steps of cellular respiration: glycolysis, the citric acid cycle, and oxidative phosphorylation.",
+    tt: 'a',
+    st: "y"
+
+};
+const options = {
+    method: 'POST',
+    body: JSON.stringify( params )  
+};
+
+fetch( 'https://api.meaningcloud.com/topics-2.0?key=c227dd46f1329bdf28e5610f3fd9cc5a&lang=en&txt=Cellular respiration is a series of chemical reactions that break down glucose to produce ATP, which may be used as energy to power many reactions throughout the body. There are three main steps of cellular respiration: glycolysis, the citric acid cycle, and oxidative phosphorylation.&tt=a&st=y', options )
+    .then( response => response.json() )
+    .then( response => {
+       console.log(response)
+    } );
+
+ // fetch('https://api.meaningcloud.com/topics-2.0')
+  //.then((response) => response.json())
+  //.then((data) => console.log(data));
+
+
   return (
     <>
       <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
